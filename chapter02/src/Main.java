40e461905f1ca38d958b12543b58dae1e238e7e4
gaoziman <2942894660@qq.com>
2024-04-09 16:13:47 +0800

    添加模块

---
+++ chapter02/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2024-04-09 16:05
+ * @description :
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
