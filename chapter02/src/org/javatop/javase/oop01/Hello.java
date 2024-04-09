63aa5bf12d4e0b1aa5b9559a07d0038b69318d73
gaoziman <2942894660@qq.com>
2024-04-09 16:23:24 +0800

    添加章节模块

---
+++ chapter02/src/org/javatop/javase/oop01/Hello.java
@@ -0,0 +1,14 @@
+package org.javatop.javase.oop01;
+
+/**
+ * @author : Leo
+ * @version 1.0
+ * @date 2024-04-09 16:21
+ * @description :
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
